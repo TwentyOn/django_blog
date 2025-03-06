b5aef0e7e1786ea0b592b7448e50061ec0bb9821
Alexey <aleksey-000@mail.ru>
2025-03-07 01:13:58 +0500

    Обновление views и метода модели get_absolute_url под SEO-url

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,8 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, publish__year=year, publish__month=month, publish__day=day, slug=post, status=Post.Status.PUBLISHED)
 
     return render(
         request,

f2643e954958fde46375776f24dd09c21fb49ece
Alexey <aleksey-000@mail.ru>
2025-03-15 01:25:45 +0500

    Добавление функционала комментирования постов

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 # Create your views here.
@@ -21,13 +22,15 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, publish__year=year, publish__month=month, publish__day=day, slug=post, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, publish__year=year, publish__month=month, publish__day=day, slug=post,
+                             status=Post.Status.PUBLISHED)
 
     return render(
         request,
         'blog/post/detail.html',
         {'post': post}
     )
+
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
@@ -47,4 +50,16 @@ def post_share(request, post_id):
             sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'form': form, 'post':post, 'sent': sent})
+    return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        cd = form.save(commit=False)
+        cd.post = post
+        cd.save()
+    return render(request, 'comment.html', {'form': form, 'post': post, 'comment': cd})

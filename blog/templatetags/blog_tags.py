3b21793110050d991848257858b1c9636c84ed1a
Aleksey <aleksey-000@mail.ru>
2025-09-06 17:51:03 +0300

    реализация пользовательского фильтра markdown для конвертирования markdown разметки в HTML-код

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -10,12 +12,17 @@ def total_posts():
     return Post.published.count()
 
 
-@register.inclusion_tag('blog/post/lates_posts.html')
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).exclude(total_comments=0).order_by(
+        '-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
 def latest_posts(count=5):
     lates_posts = Post.published.all().order_by('-publish')
     return {'latest_posts': lates_posts[:count]}
-
-
-@register.simple_tag
-def get_most_commented_posts(count=5):
-    return Post.published.annotate(total_comments=Count('comments')).exclude(total_comments=0).order_by('-total_comments')[:count]

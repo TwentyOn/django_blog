00c8b7b06b55457ac723c510c3668e8c66e1c33c
Aleksey <aleksey-000@mail.ru>
2025-09-10 21:44:29 +0300

    полноценный функционал сброса пароля для пользователя по email-адресу

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.views import generic
 from django.urls import reverse_lazy
 
 
 # Create your views here.
 class SignUpview(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'

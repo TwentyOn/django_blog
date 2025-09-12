1ac6e7a04657b638e818fd65558b6e6ad793bfd0
Aleksey <aleksey-000@mail.ru>
2025-09-12 21:53:13 +0300

    создание и регистрация модели профиля для модели User

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    biografy = models.TextField()
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username

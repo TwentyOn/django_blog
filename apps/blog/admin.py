b3a06e96c7794746646d35066daab7453bbbf060
Aleksey <aleksey-000@mail.ru>
2025-10-05 22:08:02 +0300

    базовая настройка админ панели

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Category
+from mptt.admin import DraggableMPTTAdmin
 
 # Register your models here.
+admin.site.register(Post)
+
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    prepopulated_fields = {'slug': ('title',)}

242281cb807ef8672093ebd15d731afca8fd4cfa
Aleksey <aleksey-000@mail.ru>
2025-10-25 14:07:37 +0300

    добавление навигационной панели

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProfileDetail, UserRegister, CustomLogout, CustomLogin
 
 urlpatterns = [
-    path('create_user/', UserRegister.as_view(), name='register_user'),
+    path('registration/', UserRegister.as_view(), name='registration'),
     path('login/', CustomLogin.as_view(), name='login'),
     path('logout/', CustomLogout.as_view(), name='logout'),
     path('profile/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
